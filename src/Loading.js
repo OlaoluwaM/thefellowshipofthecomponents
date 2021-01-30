85bb399d40fc77a75effa10e0d7f1b7ffbeb87e8
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2021-01-30 10:29:56 -0600

    feat: Added custom Modal and Loading components

---
+++ src/Loading.js
@@ -0,0 +1,78 @@
+import PropTypes from 'prop-types';
+
+import { m as motion } from 'framer-motion';
+import { default as styled, css } from 'styled-components';
+
+const variants = {
+  hidden: {
+    opacity: 0,
+    transition: {
+      when: 'afterChildren',
+    },
+  },
+  visible: {
+    opacity: 1,
+    transition: { when: 'beforeChildren' },
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      when: 'afterChildren',
+    },
+  },
+};
+
+const Overlay = styled(motion.div).attrs({
+  'data-testid': 'loader',
+  variants: variants,
+  initial: 'hidden',
+  animate: 'visible',
+  exit: 'exit',
+  key: 'overlay',
+  className: 'loader',
+})`
+  background: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  width: 100%;
+
+  ${({ isFullscreen }) =>
+    isFullscreen
+      ? css`
+          position: fixed;
+          height: 100%;
+        `
+      : css`
+          position: absolute;
+          height: 45%;
+        `}
+`;
+
+const Spinner = styled(motion.div)`
+  width: 30px;
+  height: 30px;
+  transform-origin: center;
+  border-radius: 100%;
+  border: 5px solid transparent;
+  border-top-color: ${({ theme }) => theme.baseColor};
+  animation: spin 0.5s linear infinite;
+  will-change: transform;
+`;
+
+export default function Loading({ fullscreen = false, layoutId, children }) {
+  const layoutObj = layoutId ? { layoutId } : { layout: true };
+
+  return (
+    <Overlay isFullscreen={fullscreen} {...layoutObj}>
+      {children ?? <Spinner layout />}
+    </Overlay>
+  );
+}
+
+Loading.propTypes = {
+  fullscreen: PropTypes.bool,
+  layoutId: PropTypes.string,
+  children: PropTypes.element,
+};
---
+++ src/Modal.js
@@ -0,0 +1,149 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import { CloseCircle } from '@styled-icons/evaicons-solid/CloseCircle';
+import { m as motion, AnimateSharedLayout } from 'framer-motion';
+
+const defaultPageTransitionVariants2 = {
+  visible: {
+    opacity: 1,
+    transition: { when: 'beforeChildren', staggerChildren: 0.1 },
+  },
+
+  hidden: {
+    opacity: 0,
+    transition: {
+      when: 'afterChildren',
+      staggerChildren: 0.1,
+    },
+  },
+
+  exit: {
+    opacity: 0,
+    transition: {
+      when: 'afterChildren',
+    },
+  },
+};
+
+const modalBackgroundVariants = {
+  popOut: {
+    opacity: 1,
+    background: rgba(0, 0, 0, 0.8),
+    transition: { ...defaultPageTransitionVariants2.visible.transition },
+  },
+  close: {
+    opacity: 0,
+    background: rgba(0, 0, 0, 0),
+    transition: { ...defaultPageTransitionVariants2.hidden.transition },
+  },
+  exit: {
+    opacity: 0,
+    background: rgba(0, 0, 0, 0),
+    transition: { ...defaultPageTransitionVariants2.hidden.transition },
+  },
+};
+
+const modalVariants = {
+  popOut: {
+    y: 0,
+    opacity: 1,
+    transition: { ...defaultPageTransitionVariants2.visible.transition },
+  },
+  close: {
+    opacity: 0,
+    y: 50,
+    transition: { ...defaultPageTransitionVariants2.hidden.transition },
+  },
+  exit: {
+    opacity: 0,
+    transition: { ...defaultPageTransitionVariants2.hidden.transition },
+  },
+};
+
+const ModalBackground = styled(motion.div).attrs({
+  variants: modalBackgroundVariants,
+  initial: 'close',
+  animate: 'popOut',
+  exit: 'exit',
+})`
+  display: block;
+  left: 0;
+  top: 0;
+  z-index: 8888;
+  background-color: rgba(0, 0, 0, 0.4);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+`;
+
+const ModalContainer = styled(motion.div).attrs({
+  variants: modalVariants,
+})`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  background: ${({ theme }) => theme.background};
+  border-radius: 5px;
+  padding: clamp(1em, 3vmin, 2em);
+  position: relative;
+  z-index: 333;
+
+  & > div {
+    overflow: hidden;
+    height: fit-content;
+  }
+
+  @media (max-width: 780px) {
+    div:not(.delete-account-modal-prompt) > &.modal-container {
+      width: 75%;
+    }
+  }
+
+  @media (max-width: 520px) {
+    div:not(.delete-account-modal-prompt) > &.modal-container {
+      width: 90%;
+    }
+  }
+`;
+
+const ModalCloseBtnSvgContainer = styled(motion.div).attrs({
+  whileTap: { scale: 0.8 },
+})`
+  position: absolute;
+  top: clamp(-16px, -14%, -5px);
+  right: clamp(-15px, -10%, -4px);
+  z-index: 100;
+
+  svg {
+    cursor: pointer;
+    fill: var(--error);
+    width: 3em;
+    max-width: 3em;
+  }
+`;
+
+export default function Modal({ className, children, closeModal }) {
+  return (
+    <ModalBackground className={`modal, ${className}`} layout>
+      <AnimateSharedLayout>
+        <ModalContainer data-testid="modal" className="modal-container" layout>
+          <ModalCloseBtnSvgContainer className="modal-close-btn-svg-container" layout="position">
+            <CloseCircle className="modal-close-btn-svg" onClick={closeModal} />
+          </ModalCloseBtnSvgContainer>
+          {children}
+        </ModalContainer>
+      </AnimateSharedLayout>
+    </ModalBackground>
+  );
+}
+
+Modal.whyDidYouRender = true;
+
+Modal.propTypes = {
+  className: PropTypes.string.isRequired,
+  closeModal: PropTypes.func.isRequired,
+};
